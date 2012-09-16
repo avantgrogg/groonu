f7e27aa4e5941391797d90512ebbd35c1b4f289f
Doug Grogg <thegroggman@gmail.com>
2012-09-15 19:15:58 -0500

    All of the initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groonu::Application.config.secret_token = 'c070d716d5761e503ad094fdccefda29f9bab1c259e1e4cf9891c42a656ca8e2521c90987e128255c06f4ca941d73ee148d5201f80e0c91f5603c2b330a94e82'
